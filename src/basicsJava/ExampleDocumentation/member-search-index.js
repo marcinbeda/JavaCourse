ba5d42420ba031b62e667dc95b8a901f22d55352
Marcin <marcin.beda@wp.pl>
2021-07-30 20:54:16 +0200

    First package, class, documentation and comments.

---
+++ src/basicsJava/ExampleDocumentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"basicsJava","c":"Comments","l":"Comments(int, int)","u":"%3Cinit%3E(int,int)"},{"p":"basicsJava","c":"Comments","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"basicsJava","c":"Comments","l":"multiply(int, int)","u":"multiply(int,int)"},{"p":"basicsJava","c":"Comments","l":"x"},{"p":"basicsJava","c":"Comments","l":"y"}];updateSearchResults();

3fd93e41d40f2c636fe41cc079004f5e4876e79d
Marcin <marcin.beda@wp.pl>
2021-07-31 13:17:40 +0200

    Lambda expressions and parameterized types.

---
+++ src/parameterizedTypes/GenericClass.java
@@ -0,0 +1,31 @@
+package parameterizedTypes;
+
+/**
+ * Created by Marcin Beda.
+ */
+
+public class GenericClass<T, V> {
+
+    private T genericVar;
+    private V secondGenericVar;
+
+    public GenericClass() {
+    }
+
+    public GenericClass(T genericVar, V secondGenericVar) {
+        this.genericVar = genericVar;
+        this.secondGenericVar = secondGenericVar;
+    }
+
+    public void nameOfGenericType() {
+        System.out.println("Typ T, to: " + genericVar.getClass().getName());
+    }
+
+    public T getGenericVar() {
+        return genericVar;
+    }
+
+    public void setGenericVar(T genericVar) {
+        this.genericVar = genericVar;
+    }
+}
---
+++ src/parameterizedTypes/SecondApplication.java
@@ -0,0 +1,15 @@
+package parameterizedTypes;
+
+/**
+ * Created by Marcin Beda.
+ */
+
+public class SecondApplication {
+    public static void main(String[] args) {
+
+        GenericClass<Double, Object> genericClass = new GenericClass<>(10.0, "Jeden");
+        genericClass.nameOfGenericType();
+
+        double value = genericClass.getGenericVar();
+    }
+}

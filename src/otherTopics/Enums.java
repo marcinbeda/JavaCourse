07031aed14ad163f69c8e44bd5bb421e9ecb3baa
Marcin <marcin.beda@wp.pl>
2021-07-31 13:37:57 +0200

    File operations, enums and streams.

---
+++ src/otherTopics/Enums.java
@@ -0,0 +1,30 @@
+package otherTopics;
+
+/**
+ * Created by Marcin Beda.
+ */
+
+public class Enums {
+
+//    public static final int BATMAN = 0;
+//    public static final int SUPERMAN = 1;
+//    public static final int WONDER_WOMAN = 2;
+
+    public static void main(String[] args) {
+
+        Superhero superhero = Superhero.BATMAN;
+
+        switch (superhero) {
+            case BATMAN:
+                break;
+            case SUPERMAN:
+                break;
+            case WONDER_WOMAN:
+                break;
+            default:
+                break;
+        }
+
+        System.out.println("Moc Batmana, to: " + Superhero.BATMAN.getPower());
+    }
+}
